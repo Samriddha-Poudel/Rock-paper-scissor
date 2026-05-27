ea2e8c874e2a32ff5be1262fb107413b2a77a9d8
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-27 20:11:10 +0545

    added the function of random choice of computer

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let container = document.querySelector('.container');
 let weaponBox =  container.querySelector('.weapon-box'); 
 let playerChoicesBox = container.querySelector('.player-choices');
 let weapons = weaponBox.querySelectorAll('.weapons div');
+let player = playerChoicesBox.querySelector(".player-choice img");
+let computer = playerChoicesBox.querySelector(".computer-choice img");
+let computerChoices =["Rock", "Paper", "Scissors" ];
 
 for (let i = 0; i < weapons.length; i++) {
     weapons[i].addEventListener("click", (e) => {
@@ -13,5 +16,20 @@ for (let i = 0; i < weapons.length; i++) {
 
             playerChoicesBox.classList.add("active");
         }, 1000);
+
+        setTimeout(() => {
+            let playerchoices = playerChoicesBox.querySelectorAll("div");
+            for(let i =0; i < playerchoices.length; i++){
+                playerchoices[i].style.animationPlayState ="paused";
+            }
+
+            player.src = e.target.src;
+
+
+            let randomChoice= computerChoices[Math.floor(Math.random() *
+          computerChoices.length)];
+          computer.src =`Images/${randomChoice}.png`;
+
+        }, 3000)
     });
 }
